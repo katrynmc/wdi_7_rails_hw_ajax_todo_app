c53646803718b7af3c1bf251631eddb50f615716
testkitschen <katryn.mcintosh@gmail.com>
2014-07-23 00:24:06 -0400

    Add get todos function to todo_list.js

--- app/assets/javascripts/todo_list.js
+++ app/assets/javascripts/todo_list.js
@@ -16,4 +16,16 @@ Collection.ToDoList = {
     this.todosListElem.empty();
     this.todosListElem.append(articlesHTML);
   },
+
+  getToDos: function(){
+    this.count = 0;
+
+    $.ajax({
+      url: 'http://localhost:3000/todos',
+    })
+    .done(this.todosCallbackHandler.bind(this))
+    .done(function(){
+this.todoCountElem.html("<p>" + this.count + " ToDos</p>");
+    }.bind(this));
+  }
 };
