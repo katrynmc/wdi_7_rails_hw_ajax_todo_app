d1bcff9c4336b0f34b48f27c672ab2db7782661a
testkitschen <katryn.mcintosh@gmail.com>
2014-07-22 23:54:46 -0400

    Add method to generate basic html structure for a todo - more refinement to be added later

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -6,3 +6,14 @@ Collection.ToDo = function(id, content, complete){
   this.content = content;
   this.complete = complete;
 };
+
+// method to generate HTML for one ToDo
+Collection.ToDo.prototype = {
+  showView: function(){
+    var todoHTML = '<li id=todo_' + this.id + '>' + this.content;
+    todoHTML += '<div>' + this.complete + '</div>';
+    todoHTML += '</li>';
+
+    return todoHTML;
+  }
+};
