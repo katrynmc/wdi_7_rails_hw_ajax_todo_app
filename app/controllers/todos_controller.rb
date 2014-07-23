5674426b9105965b40cb0c75fe6a3c6f8f840da1
testkitschen <katryn.mcintosh@gmail.com>
2014-07-23 09:50:36 -0400

    Add default serializer options and index method to todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,14 @@
 class ToDosController < ApplicationController
-  def index
+
+  def default_serializer_options
+    {root: false}
   end
+
+  respond_to :json
+
+  def index
+    @todos = ToDo.all
+    respond_with(@todos)
+  end
+
 end
