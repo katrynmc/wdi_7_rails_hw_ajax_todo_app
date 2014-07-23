b2b0caf4b9be535e3618813e4f2974466b9ffc09
testkitschen <katryn.mcintosh@gmail.com>
2014-07-22 23:51:08 -0400

    todo constructor function

---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,8 @@
+var Collection = Collection || {};
+
+// ToDo Constructor Function
+Collection.ToDo = function(id, content, complete){
+  this.id = id;
+  this.content = content;
+  this.complete = complete;
+};
